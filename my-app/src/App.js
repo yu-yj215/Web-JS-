1ecf32908a47e53610e537fa692e5844c9314036
Yeongjae-Yu <yuyoungjae@naver.com>
2023-11-18 20:41:04 +0900

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import LoginForm from './LoginForm';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Login App</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default App;
